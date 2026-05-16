a178279edbb614c965c3e6353473b1955cba5ec2
bjforson <bjforson@gmail.com>
2026-05-16 12:04:09 +0000

    Centralize scanner status clients

--- src/NickScanWebApp.New/Program.cs
+++ src/NickScanWebApp.New/Program.cs
@@ -291,6 +291,8 @@ builder.Services.AddScoped<NickScanWebApp.Shared.Services.ImageSplitterReviewCli
 builder.Services.AddScoped<NickScanWebApp.Shared.Services.XrayInspectorClient>();
 builder.Services.AddScoped<NickScanWebApp.Shared.Services.MonitoringClient>();
 builder.Services.AddScoped<NickScanWebApp.Shared.Services.LogManagementClient>();
+builder.Services.AddScoped<NickScanWebApp.Shared.Services.FS6000Client>();
+builder.Services.AddScoped<NickScanWebApp.Shared.Services.AseClient>();
 // App-specific services
 builder.Services.AddScoped<NickScanWebApp.New.Services.CargoGroupService>();
 builder.Services.AddScoped<NickScanWebApp.New.Services.CargoSummaryService>();
