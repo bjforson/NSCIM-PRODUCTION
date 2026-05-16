1732d0bc38a3b7bf3786edf212a01676f0b7de75
bjforson <bjforson@gmail.com>
2026-05-16 14:21:42 +0000

    Centralize auth bootstrap routes

---
+++ src/NickScanWebApp.Shared/Services/AuthenticationRoutes.cs
@@ -0,0 +1,8 @@
+namespace NickScanWebApp.Shared.Services;
+
+public static class AuthenticationRoutes
+{
+    public const string LoginPath = "/api/Authentication/login";
+    public const string PublicSystemStatsPath = "/api/public/system-stats";
+    public const string MyPermissionsPath = "/api/permissions/my-permissions";
+}
